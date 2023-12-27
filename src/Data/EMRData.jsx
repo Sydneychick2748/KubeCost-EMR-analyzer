4e76aef36f7c7c6b2c691d60d9a890aae05050a4
ana <ana@anniecannons.com>
2023-12-27 13:19:49 -0800

    test

---
+++ src/Data/EMRData.jsx
@@ -0,0 +1,46 @@
+// src/data/EMRData.jsx
+
+export const EMR_DATA = {
+    "j-2CDBZ8DU5A1O0": {
+      "Window": {
+        "start": "2023-12-21T00:00:00Z",
+        "end": "2023-12-22T00:00:00Z"
+      },
+      "Steps": {
+        "s-123456": {
+          "Name": "Step One",
+          "UserName": "ajay",
+          "ID": "s-123456",
+          "UserARN": "u-asdf"
+        },
+        "s-78910": {
+          "Name": "Step Two",
+          "UserName": "ajay",
+          "ID": "s-78910",
+          "UserARN": "u-asdf"
+        }
+      },
+      "AccountID": "297945954695",
+      "JobID": "j-2CDBZ8DU5A1O0",
+      "InstanceID": "",
+      "PodName": "",
+      "InstanceType": "",
+      "Cost": 2.464,
+      "Adjustment": 0.0,
+      "IdlePct": 1.0,
+      "Tags": {
+        "team": "hackathon"
+      },
+      "Timestamps": [1668896400, 1668900000, 1668903600],
+      "Resources": [
+        { "Memory": 1024 },
+        { "Memory": 1024 },
+        { "Memory": 1024 }
+      ],
+      "MemoryAllocated": 2048,
+      "MemoryUsed": 1024,
+      "Runtime": 180
+    },
+   
+  };
+  
